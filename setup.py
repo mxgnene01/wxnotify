b150600fcf18d98f843ede060d73968134676672
mengxiangguo <mengxiangguo@daling.com>
2017-07-26 18:35:29 +0800

    优化

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+#    Meng xiangguo <mxgnene01@gmail.com>
+#
+#              _____               ______
+#     ____====  ]OO|_n_n__][.      |    |]
+#    [________]_|__|________)<     |MENG|
+#     oo    oo  'oo OOOO-| oo\_   ~o~~~o~'
+# +--+--+--+--+--+--+--+--+--+--+--+--+--+
 
 from setuptools import setup
 import os
--- wxnotify/apps/message/services.py
+++ wxnotify/apps/message/services.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# This piece of code is written by
 #    Meng xiangguo <mxgnene01@gmail.com>
-# with love and passion!
 #
-#        H A P P Y    H A C K I N G !
 #              _____               ______
 #     ____====  ]OO|_n_n__][.      |    |]
 #    [________]_|__|________)<     |MENG|
